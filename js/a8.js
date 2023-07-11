6dc27bc4cf2a1a323029a746286924781b3bed69
cjk <az642008@users.noreply.github.com>
2023-07-11 16:59:57 +0800

    Site updated: 2023-07-11 16:59:55

---
+++ js/a8.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+    // waline
+    // $("#wl-nick" ).attr("placeholder" , "nickname" );
+    // $("#wl-mail" ).attr("placeholder" , "email" );
+    // $("#wl-edit" ).attr("placeholder" , "欢迎评论\r\n（审核通过后，评论才可见）" );
+}
